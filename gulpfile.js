e7f09a2173cddca78022e19d91b6e17921cadd37
wtopong <wtopong@hotmail.com>
2016-11-25 23:48:58 +1100

    Added CoffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var coffee = require('gulp-coffee');
 
-gulp.task('log', function() {
-	gutil.log('workflows are awesome');
+var coffeeSources = ['components/coffee/tagline.coffee'];
+
+gulp.task('coffee', function() {
+	gulp.src(coffeeSources)
+		.pipe(coffee({ bare: true })
+			.on('error', gutil.log))
+		.pipe(gulp.dest('components/scripts'))
+		
 });
