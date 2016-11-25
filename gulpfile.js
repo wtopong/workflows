c1e8bd276abe74e12a7d58e6db56dad4b0f4891e
wtopong <wtopong@hotmail.com>
2016-11-26 01:17:30 +1100

    Added watch task

--- gulpfile.js
+++ gulpfile.js
@@ -38,7 +38,13 @@ gulp.task('compass', function() {
 			style: 'expanded'
 		})
 		.on('error', gutil.log))
-		.pipe(gulp.dest('builds/development/js'))
+		.pipe(gulp.dest('builds/development/css'))
+});
+
+gulp.task('watch', function() {
+	gulp.watch(coffeeSources, ['coffee']);
+	gulp.watch(jsSources, ['js']);
+	gulp.watch('components/sass/*.scss', ['compass']);
 });
 
 gulp.task('default', ['coffee', 'js', 'compass']);
